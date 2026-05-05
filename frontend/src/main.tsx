96f58ee4ae2c4fb12b7d8bd931dc7c45a7c323af
olive <olivemenorah@gmail.com>
2026-05-05 17:50:42 +0300

    configure custom fonts and colours

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "@fontsource-variable/plus-jakarta-sans/wght.css";
+
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 
