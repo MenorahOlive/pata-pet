a5bc20969feec8c10f0eeed168d2fb6463919641
olive <olivemenorah@gmail.com>
2026-05-07 15:47:04 +0300

    update menubar to have active state icons

--- frontend/src/components/layout/MenuBar.tsx
+++ frontend/src/components/layout/MenuBar.tsx
@@ -1,18 +1,31 @@
 import { PawPrint, Send, Heart, UserRound } from "lucide-react";
 
 const links = [
-  { title: "Discover", icon: PawPrint },
-  { title: "Messages", icon: Send },
-  { title: "Favorites", icon: Heart },
-  { title: "Profile", icon: UserRound },
+  { title: "Discover", icon: PawPrint, href: "/discover" },
+  { title: "Messages", icon: Send, href: "/messages" },
+  { title: "Favorites", icon: Heart, href: "/messages" },
+  { title: "Profile", icon: UserRound, href: "/messages" },
 ];
 
-const LinkLayout = ({ title, icon: Icon }: { title: string; icon: any }) => {
+const LinkLayout = ({
+  title,
+  icon: Icon,
+  href,
+  isActive,
+}: {
+  title: string;
+  icon: any;
+  href: string;
+  isActive: boolean;
+}) => {
   return (
-    <div className="flex flex-col items-center gap-1 text-black-custom/45">
+    <a
+      className={`flex flex-col items-center gap-1 text-black-custom/45  ${isActive ? "active:bg-orange-custom/45" : "bg-transparent"}`}
+      href={href}
+    >
       <Icon size={18} />
       <span className="text-sm">{title}</span>
-    </div>
+    </a>
   );
 };
 
@@ -20,7 +33,13 @@ const MenuBar = () => {
   return (
     <div className="w-full bg-white-custom p-5 flex justify-between items-center rounded-t-3xl border-t-2  border-orange-custom/7 z-10 fixed bottom-0 shadow-2xl shadow-orange-custom">
       {links.map((l, index) => (
-        <LinkLayout key={index} title={l.title} icon={l.icon} />
+        <LinkLayout
+          key={index}
+          title={l.title}
+          icon={l.icon}
+          href={l.href}
+          isActive={false}
+        />
       ))}
     </div>
   );
