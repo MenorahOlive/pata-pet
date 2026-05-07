966632e8b2cda318b0f7a83b0c5127b0e6e9abe9
olive <olivemenorah@gmail.com>
2026-05-07 15:48:33 +0300

    create hero section of  welcome page

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -9,7 +9,8 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/80",
+        default: "bg-orange-custom text-white-custom hover:bg-orange-custom/80",
+        green: "bg-green-custom text-white-custom hover:bg-green-custom/50",
         outline:
           "border-border bg-input/30 hover:bg-input/50 hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground",
         secondary:
@@ -25,7 +26,7 @@ const buttonVariants = cva(
           "h-9 gap-1.5 px-3 has-data-[icon=inline-end]:pr-2.5 has-data-[icon=inline-start]:pl-2.5",
         xs: "h-6 gap-1 px-2.5 text-xs has-data-[icon=inline-end]:pr-2 has-data-[icon=inline-start]:pl-2 [&_svg:not([class*='size-'])]:size-3",
         sm: "h-8 gap-1 px-3 has-data-[icon=inline-end]:pr-2 has-data-[icon=inline-start]:pl-2",
-        lg: "h-10 gap-1.5 px-4 has-data-[icon=inline-end]:pr-3 has-data-[icon=inline-start]:pl-3",
+        lg: "h-12 gap-1.5 px-4 has-data-[icon=inline-end]:pr-3 has-data-[icon=inline-start]:pl-3",
         icon: "size-9",
         "icon-xs": "size-6 [&_svg:not([class*='size-'])]:size-3",
         "icon-sm": "size-8",
--- frontend/src/pages/WelcomePage.tsx
+++ frontend/src/pages/WelcomePage.tsx
@@ -1,10 +1,39 @@
-import MainLayout from "../components/layout/MainLayout";
+import { Button } from "../components/ui/button";
+import { CirclePlay } from "lucide-react";
+import { PawPrint } from "lucide-react";
+import catanddog2 from "../assets/cat-and-dog-2.jpg";
 const WelcomePage = () => {
   return (
     <>
-      <MainLayout>
-        <div className=""></div>
-      </MainLayout>
+      <section className="p-5 text-black-custom flex flex-col gap-8">
+        <span className="flex flex-row items-center  justify-center gap-2 text-4xl text-orange-custom font-extrabold">
+          <PawPrint className="size-8" />
+          PataPet
+        </span>
+        <h1 className="text-5xl font-medium text-center mt-5">
+          Find your new{" "}
+          <span className="text-orange-custom font">best friend</span>
+        </h1>
+        <p className="text-center">
+          Discover local pets waiting for a loving home. Our gentle matchmaking
+          process helps you find the companion that fits your life perfectly.
+        </p>
+        <div className="flex flex-col gap-5">
+          {" "}
+          <Button size="lg">Get Started</Button>
+          <Button
+            className="text-[#6e8670] bg-[#ceeacf]"
+            size="lg"
+            variant="green"
+          >
+            <CirclePlay />
+            How it works
+          </Button>
+        </div>
+        <div>
+          <img src={catanddog2} alt="Image of a cat and dog" />
+        </div>
+      </section>
     </>
   );
 };
